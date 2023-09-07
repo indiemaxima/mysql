a63d734ccb8ed8b64fd58dbd91f217b69624fa29
indiemaxima <128857708+indiemaxima@users.noreply.github.com>
2023-09-07 18:04:26 -0500

    Solo Registros
    Inserta datos de ejemplo a la base de datos Colegio.  No hay cambios en el modelo de datos.

---
+++ Colegio_Solo_Registros.sql
@@ -0,0 +1,23 @@
+USE `colegio`;
+
+INSERT INTO `alumnos` VALUES (1,'Juan','Perez');
+INSERT INTO `alumnos` VALUES (2,'Pedro','Rodriguez');
+INSERT INTO `alumnos` VALUES (3,'Lucas','Velandia');
+
+INSERT INTO `datos_personales` VALUES (1,1,'Calle 198','3005454','Maria Forero');
+INSERT INTO `datos_personales` VALUES (2,2,'Carrera 28 # 80 - 35','2704162','Bernardo Garcia');
+INSERT INTO `datos_personales` VALUES (3,3,'Transversal 50 - 35 int 20 bl 3','315894565','Jose Garcia');
+
+INSERT INTO `materias` VALUES (1,'Matematicas','Rodolfo');
+INSERT INTO `materias` VALUES (2,'Geografia','Christian');
+INSERT INTO `materias` VALUES (3,'Ingles','Rosario');
+
+INSERT INTO `calificaciones` VALUES (1,1,1,3.2,1);
+INSERT INTO `calificaciones` VALUES (2,1,2,5,1);
+INSERT INTO `calificaciones` VALUES (3,1,3,4,1);
+INSERT INTO `calificaciones` VALUES (4,2,1,1.7,1);
+INSERT INTO `calificaciones` VALUES (5,2,2,4.5,1);
+INSERT INTO `calificaciones` VALUES (6,2,3,3.1,1);
+INSERT INTO `calificaciones` VALUES (7,3,1,4,1);
+INSERT INTO `calificaciones` VALUES (8,3,2,5,1);
+INSERT INTO `calificaciones` VALUES (9,3,3,1,1);
